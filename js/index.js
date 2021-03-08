d1f68f8e08ad0d987ac27cbfffd0976cf57a7b3c
Francis Nguyen <francishtknguyen@gmail.com>
2021-03-08 17:07:11 -0600

    finished header and working on cta and main

--- js/index.js
+++ js/index.js
@@ -40,3 +40,67 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const header = document.querySelector("header");
+const cta = document.querySelector(".cta");
+const main = document.querySelector(".main-content");
+const contact = document.querySelector(".contact");
+
+const links = header.querySelectorAll("nav a");
+const ctaImg = cta.querySelector("#cta-img")
+const ctaButton =  cta.querySelector("button");
+const mainTop = main.querySelectorAll(".top-content .text-content");
+const mainBot = main.querySelectorAll(".bottom-content .text-content");
+
+
+// Creating Navigation links
+links[0].textContent = "Services";
+links[1].textContent = "Product";
+links[2].textContent = "Vision";
+links[3].textContent = "Features";
+links[4].textContent = "About";
+links[5].textContent = "Contact";
+
+// Creating CTA content
+const ctaTitle = cta.querySelector("h1");
+ctaTitle.textContent = "DOM is Awesome";
+ctaButton.textContent = "Get Started";
+ctaImg.src = "img/header-img.png";
+
+// Main content
+
+const mainImg = main.querySelector(".middle-img");
+
+mainImg.src = "img/mid-page-accent.jpg";
+
+const topTitle1 = mainTop[0].querySelector("h4")
+const topCont1 =  mainTop[0].querySelector("p");
+
+topTitle1.textContent = "Features";
+topCont1.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+const topTitle2 = mainTop[1].querySelector("h4")
+const topCont2 =  mainTop[1].querySelector("p");
+
+topTitle2.textContent = "About";
+topCont2.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+const botTitle1 = mainBot[0].querySelector("h4")
+const botCont1 =  mainBot[0].querySelector("p");
+
+botTitle1.textContent = "Services";
+botCont1.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+const botTitle2 = mainBot[1].querySelector("h4")
+const botCont2 =  mainBot[1].querySelector("p");
+
+botTitle2.textContent = "Product";
+botCont2.textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+const botTitle3 = mainBot[2].querySelector("h4")
+const botCont3 =  mainBot[2].querySelector("p");
+
+botTitle3.textContent = "Vision";
+botCont3.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+
